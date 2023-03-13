2b302ff4fffc73e9d413d2fc376d14e2bfa54a00
Adam McKee <adam.be.g84d@gmail.com>
2023-03-12 22:19:30 -0500

    skip migrated cql files on subsequent migrates

--- src/migrate.rs
+++ src/migrate.rs
@@ -17,22 +17,40 @@ pub(crate) async fn perform(
     cql_files: Vec<CqlFile>,
     args: MigrateArgs,
 ) -> Result<Vec<CqlFile>> {
-    for cql_file in &cql_files {
+    let previously_migrated = select_migrated_cql_filenames(session, &args).await?;
+    let mut migrated: Vec<CqlFile> = Vec::new();
+    for cql_file in cql_files {
+        if previously_migrated.contains(&cql_file.filename) {
+            continue;
+        }
         for cql_statement in read_statements(&PathBuf::from(&cql_file.filename))? {
             // println!("\n---\n{cql_statement}\n---");
             session.query(cql_statement, ()).await?;
         }
-
         queries::migrated::files::insert(
             session,
             &args.history_keyspace,
             &args.history_table,
-            cql_file,
+            &cql_file,
         )
         .await?;
+        migrated.push(cql_file);
     }
+    Ok(migrated)
+}
 
-    Ok(cql_files)
+async fn select_migrated_cql_filenames(
+    session: &Session,
+    args: &MigrateArgs,
+) -> Result<Vec<String>> {
+    let migrated_cql_files =
+        queries::migrated::files::select_all(session, &args.history_keyspace, &args.history_table)
+            .await?;
+    let mut result = Vec::new();
+    for migrated_cql_file in migrated_cql_files {
+        result.push(migrated_cql_file.filename);
+    }
+    Ok(result)
 }
 
 fn read_statements(cql_file: &PathBuf) -> Result<Vec<String>> {
